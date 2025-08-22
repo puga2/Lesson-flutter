f735cd2a8491aeee462d4ba92ccfeb21bfcb7b44
puga2 <o.kimhong168@gmail.com>
2025-08-22 16:14:15 +0700

    dropdown button done

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -12,7 +12,7 @@ class _ProfilePageState extends State<ProfilePage> {
   bool? isChecked = false;
   bool isSwitched = false;
   double sliderValue = 0.0;
-
+  String? menuItem;
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -21,6 +21,19 @@ class _ProfilePageState extends State<ProfilePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            DropdownButton(
+              value: 'e3',
+              items: [
+                DropdownMenuItem(value: 'e1', child: Text("Element1")),
+                DropdownMenuItem(value: 'e2', child: Text("Element2")),
+                DropdownMenuItem(value: 'e3', child: Text("Element3")),
+              ],
+              onChanged: (String? value) {
+                setState(() {
+                  menuItem = value;
+                });
+              },
+            ),
             TextField(
               controller: controller,
               decoration: InputDecoration(
@@ -78,15 +91,41 @@ class _ProfilePageState extends State<ProfilePage> {
                 print(value);
               },
             ),
-            GestureDetector(
+            InkWell(
+              splashColor: Colors.blue,
               onTap: () {
                 setState(() {
                   sliderValue = 50.0;
-           
                 });
+                print('image selected!');
               },
-              child: Image.asset('assets/images/bg.jpg'),
+              child: Container(
+                height: 200,
+                width: double.infinity,
+                color: Colors.white24,
+              ),
             ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.teal,
+                foregroundColor: Colors.white,
+              ),
+              child: Text('Click me'),
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('Click me')),
+            FilledButton(
+              onPressed: () {},
+              style: FilledButton.styleFrom(
+                backgroundColor: Colors.amber,
+                foregroundColor: Colors.black,
+              ),
+              child: Text('Click Me'),
+            ),
+            TextButton(onPressed: () {}, child: Text('Text Btn')),
+            OutlinedButton(onPressed: () {}, child: Text('Outline Btn')),
+            CloseButton(),
+            BackButton(),
           ],
         ),
       ),
